4c644b7dd290af6c6c728fb92037c125f109be78
Ubuntu <ubuntu@williams.medina@umine.com>
2022-12-27 15:12:49 +0000

    fixed filenames

--- database_scripts/download_databases_from_S3.py
+++ database_scripts/download_databases_from_S3.py
@@ -2,7 +2,7 @@ import boto3
 import os
 from dotenv import load_dotenv
 
-load_dotenv() 
+load_dotenv()
 bucket_name = os.environ.get('S3_BUCKET_NAME')
 access_key = os.environ.get('S3_ACCESS_KEY')
 secret_key = os.environ.get('S3_SECRET_KEY')
@@ -18,4 +18,3 @@ def download_files_from_s3(pathToSaveFiles):
             s3_session.meta.client.download_file(bucket_name,content_name, pathToSaveFiles + content_name)
             print(content_name)
 download_files_from_s3(pathToSaveFiles = "/home/ubuntu/DataPanel/pages/Database/")
-
